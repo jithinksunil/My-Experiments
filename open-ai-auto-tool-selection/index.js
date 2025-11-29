1fb528d64f4628271a1b748f882f5d9a03243dad
Jithin K Sunil <jithinksunil96@gmail.com>
2025-11-29 10:22:49 +0530

    updatea

--- open-ai-auto-tool-selection/index.js
+++ open-ai-auto-tool-selection/index.js
@@ -1,25 +1,57 @@
 import OpenAI from 'openai';
 import dotenv from 'dotenv';
+import { zodTextFormat } from 'openai/helpers/zod';
+import { z } from 'zod';
 dotenv.config();
 const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 async function run() {
-  const response = await client.responses.create({
-    model: 'gpt-5.1', // Best for tool use
-    input: 'Find the latest news about Tesla and summarize it.',
+  // const data = fs.readFileSync('pitchdeck.pdf');
+  // const base64String = data.toString('base64');
+  // const file = await client.files.create({
+  //   file: fs.createReadStream('pitchdeck.pdf'),
+  //   purpose: 'user_data',
+  // });
 
-    tools: [
-      { type: 'web_search' }, // lets model search the web
-      { type: 'deep_research' }, // long-form reasoning
-      { type: 'code_interpreter' }, // python execution
-      { type: 'file_search' }, // for file-based tasks
-    ],
-
-    // optional — allows the model to choose ANY tool or NONE
-    tool_choice: 'auto',
+  const Details = z.object({
+    details: z.string(),
   });
 
-  console.log(JSON.stringify(response.output[0].content, null, 2));
+  const stream = await client.responses.create({
+    model: 'gpt-5-nano', // Best for tool use
+    text: {
+      format: zodTextFormat(Details, 'details'),
+    },
+    input: [
+      {
+        role: 'user',
+        content: [
+          {
+            type: 'input_file',
+            file_id: 'file-2ene4X3QUnpqBzRCkYoXEX',
+          },
+          {
+            type: 'input_text',
+            text: 'What is the document about? Who are the founders, check if the company is still running or not and give current status',
+          },
+        ],
+      },
+    ],
+    reasoning: { effort: 'low' },
+    tools: [
+      { type: 'web_search', search_context_size: 'high' }, // lets model search the web
+      //   { type: 'code_interpreter' }, // python execution
+    ],
+    // optional — allows the model to choose ANY tool or NONE
+    tool_choice: 'auto',
+    stream: true,
+  });
+  for await (const event of stream) {
+    // console.log(event);
+    if (event.type === 'response.output_text.delta') {
+      process.stdout.write(event.delta);
+    }
+  }
 }
 
 run();
---
+++ open-ai-auto-tool-selection/streaming.js
@@ -0,0 +1,22 @@
+import OpenAI from 'openai';
+import dotenv from 'dotenv';
+dotenv.config();
+const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+const stream = await client.responses.create({
+  model: 'gpt-5',
+  input: [
+    {
+      role: 'user',
+      content: "Tell a story",
+    },
+  ],
+  stream: true,
+});
+console.log('reached');
+
+for await (const event of stream) {
+  if (event.type === 'response.output_text.delta') {
+    process.stdout.write(event.delta);
+  }
+}
